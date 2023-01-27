c128a499205aa647e38ec53edb001d2bae3c9257
ebouchar31 <bouchard.e11@hotmail.com>
2023-01-26 23:08:47 -0500

    code de base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Objetsperdus from './Components/ObjetsPerdus';
+
+
+function App() {
+  return (
+    <div className="eventask">
+      <Objetsperdus/>
+    </div>
+  );
+}
+
+export default App;
