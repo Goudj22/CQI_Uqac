7f36251851d51b01d332b7c10e20c5dc650d400f
ebouchar31 <bouchard.e11@hotmail.com>
2023-01-26 23:48:57 -0500

    signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Connexion from './Components/Connexion';
+import Inscription from './Components/Inscription';
 import Objetsperdus from './Components/ObjetsPerdus';
 
 
 function App() {
   return (
     <div className="eventask">
+      <Connexion/>
+      <Inscription/>
       <Objetsperdus/>
+      
     </div>
   );
 }
